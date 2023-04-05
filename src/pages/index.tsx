32ffabcb7967b520aa9bdf8657903c540e591f29
zer0 <zer0@zer0.com>
2023-04-06 07:40:05 +0900

    init css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
     return (
-        <>
-            <main>
-                <h1 className="text-4xl font-bold text-gray-900 bg-red-400">
-                    hello world
-                </h1>
-            </main>
-        </>
+        <main>
+            <h1 className="text-4xl font-bold text-gray-900 bg-red-400">
+                hello world
+            </h1>
+        </main>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    mode: "jit",
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
