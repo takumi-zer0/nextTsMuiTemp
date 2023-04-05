298886fa2ed37d51de82b5fbafd8cc61d843a0ac
zer0 <zer0@zer0.com>
2023-04-06 07:35:25 +0900

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+            <main>
+                <h1 className="text-4xl font-bold text-gray-900 bg-red-400">
+                    hello world
+                </h1>
+            </main>
+        </>
+    );
+}
